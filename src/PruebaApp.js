b58ded3e7c3e3a551761beae34f90610bb276b18
RamirezJosue <xdemobtkx@gmail.com>
2021-06-04 11:52:22 -0500

    Inicio con tailwind css

---
+++ src/PruebaApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+export const PruebaApp = () => {
+    return (
+        <nav className="bg-gray-800">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="relative flex items-center justify-between h-16">
+
+                </div>
+            </div>
+        </nav>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PruebaApp } from './PruebaApp';
+
+
+ReactDOM.render(
+    <PruebaApp />,
+  document.getElementById('root')
+);
